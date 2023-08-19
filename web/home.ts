966fabc11a16a853760ab05f1d49488758586c04
Daniel_Haupt <danielh@bbd.co.za>
2023-08-19 16:03:15 +0200

    removed secret

--- web/home.ts
+++ web/home.ts
@@ -1,17 +1,41 @@
-const cognitoDomain = "https://gotta-save-them-all.auth.us-east-1.amazoncognito.com";
-const clientID = "351ntcgkgo32jeh3vqrbblb6i0";
-const clientSecret = "g3oll68h113i9f3uj77k3hmb0fnb59vj5g9o2p9v7ng44u2689";
-const base64Credentials = btoa(`${clientID}:${clientSecret}`);
-const basicAuthorization = `Basic ${base64Credentials}`;
+const cognitoDomain = "https://gotta-save-them-all-login.auth.us-east-1.amazoncognito.com";
+const clientID = "5uc5eknh95m4golenj8208sueo";
 
 var urlParams = new URLSearchParams(window.location.search);
 var code = urlParams.get('code');
 
-if (code) {
-    exchangeCodeForTokens(code);
+exchangeCodeForTokens()
+getUserInfoExample()
 
-    const token = localStorage.getItem("gottaSaveThemAllToken");
+async function exchangeCodeForTokens() {
+    if (code) {
+        const headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            // "Authorization": basicAuthorization,
+        };
     
+        const data = new URLSearchParams();
+        data.append("grant_type", "authorization_code");
+        data.append("client_id", clientID);
+        data.append("code", code);
+        data.append("redirect_uri", "http://localhost:8080/");
+    
+        const response = await fetch(`${cognitoDomain}/oauth2/token?${data.toString()}`, {
+            method: 'POST',
+            headers: headers,
+        });
+       
+        if (response.status != 200) return;
+        const body = new Response(response.body);
+        body.json().then((data) => {
+            localStorage.setItem('gottaSaveThemAllToken', data.access_token);
+            localStorage.setItem('gottaSaveThemAllRefreshToken', data.refresh_token);
+        })
+    }
+}
+
+function getUserInfoExample() {
+    const token = localStorage.getItem("gottaSaveThemAllToken");
     if (token) {
         const userInfoHeaders = {
             Authorization: "Bearer " + token,
@@ -29,28 +53,3 @@ if (code) {
         })
     }
 }
-
-function exchangeCodeForTokens(code: string) {
-    const headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Authorization": basicAuthorization,
-    };
-
-    const data = new URLSearchParams();
-    data.append("grant_type", "authorization_code");
-    data.append("client_id", clientID);
-    data.append("code", code);
-    data.append("redirect_uri", "http://localhost:8080/");
-
-    fetch(`${cognitoDomain}/oauth2/token?${data.toString()}`, {
-        method: 'POST',
-        headers: headers,
-    }).then((res) => {
-        if (res.status != 200) return;
-        const response = new Response(res.body);
-        response.json().then((data) => {
-            localStorage.setItem('gottaSaveThemAllToken', data.access_token);
-            localStorage.setItem('gottaSaveThemAllRefreshToken', data.refresh_token);
-        })
-    })
-}  
