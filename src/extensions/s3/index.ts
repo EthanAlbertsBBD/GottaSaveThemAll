dd22fd6c54b46574c41a9e5c0c361770105431d2
Marius de Jager <mariusdj@bbd.co.za>
2023-08-20 00:40:30 +0200

    Added getS3Object

---
+++ src/extensions/s3/index.ts
@@ -0,0 +1,27 @@
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+import { Readable } from 'stream';
+import config from 'config';
+
+const client = new S3Client({ region: config.get('REGION')! });
+
+const streamToString = (stream): Promise<string> => new Promise((resolve, reject) => {
+  const chunks: Buffer[] = []
+  stream.on('data', (chunk:any) => chunks.push(chunk));
+  stream.on('error', reject);
+  stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+});
+
+export const getS3Object =  (bucket:string, key:string) => {
+  const params = {
+    Bucket: bucket,
+    Key: key
+  };
+  const command = new GetObjectCommand(params);
+  return client.send(command)
+    .then((response: any) => {
+      const stream = response.Body as Readable;
+      return streamToString(stream).then((result: string) => {
+        return JSON.parse(result);
+      });
+    });
+}
