27bc867d3b442f812cc5524e1bfbb7bd423efb41
Morgan Moss <morgan@bbd.co.za>
2023-09-07 22:20:06 +0200

    Colleciton Page, no API

--- web/scripts/pokemon.ts
+++ web/scripts/pokemon.ts
@@ -2,6 +2,15 @@ let showPokemonCard = false;
 
 showCard();
 
+const card_items_reference = {
+  hp: "pokemon-hp",
+  art: "pokemon-art",
+  types: "pokemon-type",
+  attack: "pokemon-attack",
+  defence: "pokemon-defense",
+  speed: "pokemon-speed"
+}
+
 function showCard() {
   const pokemonCard = document.getElementById('pokemonCard');
 
@@ -38,3 +47,116 @@ async function getPokemon() {
 }
 
 function setUpPokemonCard() {}
+
+async function getCollection() {
+  const token = localStorage.getItem('gottaSaveThemAllToken');
+  const pokemonData = [{
+    id: 683,
+    name: 'Aromatisse',
+    abilities: 'HealerAroma Veil',
+    hp: '101',
+    attack: '72',
+    defense: '72',
+    speedAttack: '99',
+    speedDefense: '89',
+    speed: '29',
+    types: ['fairy'],
+    artImage: 'images/art/683.png',
+    iconImage: 'images/icons/683.png',
+  },{
+    id: 683,
+    name: 'Aromatisse',
+    abilities: 'HealerAroma Veil',
+    hp: '101',
+    attack: '72',
+    defense: '72',
+    speedAttack: '99',
+    speedDefense: '89',
+    speed: '29',
+    types: ['fairy'],
+    artImage: 'images/art/683.png',
+    iconImage: 'images/icons/683.png',
+  },{
+    id: 683,
+    name: 'Aromatisse',
+    abilities: 'HealerAroma Veil',
+    hp: '101',
+    attack: '72',
+    defense: '72',
+    speedAttack: '99',
+    speedDefense: '89',
+    speed: '29',
+    types: ['fairy'],
+    artImage: 'images/art/683.png',
+    iconImage: 'images/icons/683.png',
+  },{
+    id: 683,
+    name: 'Aromatisse',
+    abilities: 'HealerAroma Veil',
+    hp: '101',
+    attack: '72',
+    defense: '72',
+    speedAttack: '99',
+    speedDefense: '89',
+    speed: '29',
+    types: ['fairy'],
+    artImage: 'images/art/683.png',
+    iconImage: 'images/icons/683.png',
+  },{
+    id: 683,
+    name: 'Aromatisse',
+    abilities: 'HealerAroma Veil',
+    hp: '101',
+    attack: '72',
+    defense: '72',
+    speedAttack: '99',
+    speedDefense: '89',
+    speed: '29',
+    types: ['fairy'],
+    artImage: 'images/art/683.png',
+    iconImage: 'images/icons/683.png',
+  }];
+  
+  Array.from(document.getElementsByClassName("capture-pokemon-button")).forEach(element => {
+    element.style.display = 'none';
+  });
+  const reference_card: HTMLElement | null = document.getElementById('pokemonCard');
+  const collection: HTMLElement | null = document.getElementById('collection');
+
+  if (!reference_card || !collection) {
+    return;
+  }
+  reference_card.style.display = 'none';
+  pokemonData.forEach((card) => {
+    console.log("HERE", card)
+    const copy: HTMLElement = (reference_card.cloneNode(true))
+    copy.id = ""
+    Array.from(copy.children).forEach((child: Element) => {
+      if (child.classList.contains(card_items_reference.art)){
+        child.textContent = card.artImage;
+        console.log("Art")
+      }
+      if (child.classList.contains(card_items_reference.hp)){
+        child.textContent = "HP: " + card.hp;
+      }
+      if (child.classList.contains(card_items_reference.attack)){
+        child.textContent = "Attack: " + card.attack;
+      }
+      if (child.classList.contains(card_items_reference.defence)){
+        child.textContent = "Defense: " + card.defense;
+      }
+      if (child.classList.contains(card_items_reference.speed)){
+        child.textContent = "Speed: " + card.speed;
+      }
+      if (child.classList.contains(card_items_reference.types)){
+        child.textContent = "Types: " + card.types.join(" ").toUpperCase();
+      }
+    });
+    copy.style.display = 'block'
+    collection.appendChild(copy)
+  });
+}
+
+
+  //   
+  // })
